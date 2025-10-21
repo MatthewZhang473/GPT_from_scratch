6ff033352850ff6ef303291aa6067975fd11142e
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2025-10-21 11:28:17 +0100

    clean up + enabled large scale training (10M model)

--- gpt.py
+++ gpt.py
@@ -3,28 +3,30 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 # hyperparameters
-batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
-max_iters = 5000
-eval_interval = 300
-learning_rate = 1e-2
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200
-n_embd = 32
-n_layers = 4
-n_head = 4
-dropout = 0.05
-# batch_size = 64 # how many independent sequences will we process in parallel?
-# block_size = 256 # what is the maximum context length for predictions?
+
+# batch_size = 32
+# block_size = 8
 # max_iters = 5000
-# eval_interval = 500
-# learning_rate = 3e-4
+# eval_interval = 300
+# learning_rate = 1e-2
 # device = 'cuda' if torch.cuda.is_available() else 'cpu'
 # eval_iters = 200
-# n_embd = 384 # 6 heads * 64 dimensions = 384
-# n_layers = 6
-# n_head = 6
-# dropout = 0.2
+# n_embd = 32
+# n_layers = 4
+# n_head = 4
+# dropout = 0.05
+
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
+max_iters = 5000
+eval_interval = 500
+learning_rate = 3e-4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 384 # 6 heads * 64 dimensions = 384
+n_layers = 6
+n_head = 6
+dropout = 0.2
 # ------------
 
 torch.manual_seed(1337)
